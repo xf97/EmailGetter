c3ffe7df83836e6e0d264158e9d5deee2142f7b9
xiaofeng <1506010130@hhu.edu.cn>
2020-05-26 09:39:10 +0800

    update code

--- getEmail.py
+++ getEmail.py
@@ -1,15 +1,24 @@
 #!/usr/bin/python
 #-*- coding:utf-8 -*-
 
+
+'''
+__author__ = xiaofeng
+'''
+
 import requests
+import os
+import platform
+import sys
+
 
 
 
 class getEmail:
 	def __init__(self, _tokenFile):
 		self.page = 1
-		self.serach_str = "https://api.github.com/users/perfectmak"
-		#self.serach_str = "https://api.github.com/search/users?q=smart+contract"
+		#self.serach_str = "https://api.github.com/users/perfectmak"
+		self.serach_str = "https://api.github.com/search/users?q=smart+contract"
 		self.token = self.getToken(_tokenFile)
 		self.headers = {'User-Agent':'Mozilla/5.0', 'Authorization':'token ' + str(self.token), 'Content-Type':'application/json', 'method':'GET', 'Accept':'application/json'}
 		
@@ -17,33 +26,39 @@ class getEmail:
 		f = open(_tokenFile, "r", encoding = "utf-8")
 		token = f.read()
 		f.close()
-		return token
+		if "\n" in token:
+			return token[:-1]
+		else:
+			return token
 
 	def getText(self):
 		r = requests.get(self.serach_str, timeout = 50, headers = self.headers)
 		r.raise_for_status()
 		r.encoding = r.apparent_encoding
-		return r.text
+		writeTxt("usersList.txt", r.text)
 
 	def makeUrl(self):
 		self.page += 1
 		self.serach_str = "https://github.com/search?p="+str(self.page)+"&q=smart+contract&type=Users"
 
-	def run(self, _userNum):
-		if _userNum % 10 == 0:
-			loopNum = _userNum // 10 
-		else:
-			loopNum = _userNum // 10 + 1
-		for i in range(1, loopNum):
-			self.makeUrl()
-			print(self.serach_str)
+	def writeTxt(self, _filename, _content):
+		f = open(_filename, "w", encoding = "utf-8")
+		f.write(_content)
+		print("File " +_filename+ " has been written.")
+		f.close()
+
+	def run(self):
+		self.getText()
+		
+
+def getTokenFile():
+	if "windows" in platform.system() or "Windows" in platform.system():
+		return "token_win10.txt"
+	elif "Linux" in platform.system():
+		return "token_linux.txt"
 
 
 #unit test
 if __name__ == "__main__":
-	g = getEmail("token_win10.txt")
-	text = g.getText()
-	f = open("html.txt", "w", encoding = "utf-8")
-	f.write(text)
-	f.close()
-	#g.run(658)
+	g = getEmail(getTokenFile())
+	text = g.run()
